c8050758dbe74de64d77101be2e10619f3bd081e
mercedes <mechgalera@gmail.com>
2019-05-20 12:58:44 +0200

    conexion hecha

--- models/validatedata.js
+++ models/validatedata.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //para conectar con una sola base de datos, en caso de que fuesen dos tendríamos que poner mongoose.createConnection
 
-mongoose.connect('mongodb://localhost/databasename')
+mongoose.connect('mongodb+srv://root:toor@cluster0-clvxs.gcp.mongodb.net/hotm?retryWrites=true')
     .then(() => console.log('Now connected to MongoDB!'))
     .catch(err => console.error('Something went wrong', err));
 
@@ -43,4 +43,4 @@ const studiesSchema =new mongoose.Schema({
 
 
 
-module.exports = mongoose.model('Thing', thingSchema);
+module.exports = {studiesSchema, stakeholderSchema, migrantSchema};
